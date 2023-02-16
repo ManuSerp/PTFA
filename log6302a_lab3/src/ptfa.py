9f5c1afd7f98ef94dada346c4a4b438ecf9fda67
Serp <manu.serpette74@gmail.com>
2023-02-16 14:40:18 -0500

    ptfa

---
+++ log6302a_lab3/src/ptfa.py
@@ -0,0 +1,57 @@
+from code_analysis import *
+import time
+
+
+class CFGA:
+    def __init__(self):
+        self.cfg = None
+        self.IN = []
+        self.OUT = []
+        self.nodeset = None
+
+    def poss(IN, OUT, node, parent):
+        return IN[node] or OUT[parent]
+
+    def definit(IN, OUT, node, parent):
+        return IN[node] and OUT[parent]
+
+    def ptfa(self, cfg: CFG, func_comp, pattern_set=["Pattern"]):
+        t1 = time.time()
+        # init
+        self.cfg = cfg
+        nodeid = self.cfg.get_node_ids()
+        self.IN = [False] * len(nodeid)
+        self.OUT = [False] * len(nodeid)
+        self.oldOUT = [False] * len(nodeid)
+        self.nodeset = nodeid
+        changes = True
+        # loop
+        while changes:
+            changes = False
+            for node in self.nodeset:
+                parents = self.cfg.get_any_parents(node)
+                for parent in parents:
+                    self.IN[node] = func_comp(self.IN, self.OUT, node, parent)
+                self.oldOUT[node] = self.OUT[node]
+                if self.cfg.get_type(node) in pattern_set:
+                    self.OUT[node] = True
+                else:
+                    self.OUT[node] = self.IN[node]
+                if self.oldOUT[node] != self.OUT[node]:
+                    changes = True
+
+        result = []
+        for i in range(len(self.nodeset)):
+            if self.OUT[i]:
+                result.append(self.nodeset[i])
+        t2 = time.time()-t1
+        return result, t2
+
+
+if __name__ == '__main__':
+
+    cfgreader = CFGReader()
+
+    cfg = cfgreader.read_cfg("../tp/perf/graph1.cfg.json")
+    cfga = CFGA()
+    print(cfga.ptfa(cfg, CFGA.poss))
